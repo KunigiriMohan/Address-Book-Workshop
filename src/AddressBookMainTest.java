17d01755f0b731d1042312a07a5290ac121cb7c8
KunigiriMohan <90668633+KunigiriMohan@users.noreply.github.com>
2021-11-29 17:18:18 +0530

    Added : Test class for testing methods to check person present by city and state.

---
+++ src/AddressBookMainTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressBookMainTest {
+
+    /*@Test
+    void add() {
+        AddressBookMain obj = new AddressBookMain();
+        obj.add("mohan", "mohan", "raju", "8555058332", "adoni", "andhra", "518301");
+        assertFalse((obj.add("mohan", "mohan", "raju", "8555058332", "adoni", "andhra", "518301")));
+    }*/
+
+    @Test
+    void add1() {
+        AddressBookMain obj = new AddressBookMain();
+        obj.add("mohan", "mohan", "raju", "8555058332", "adoni", "andhra", "518301");
+        assertEquals(true,obj.personbyState("andhra"));
+    }
+    @Test
+    void add2(){
+        AddressBookMain obj = new AddressBookMain();
+        obj.add("mohan", "mohan", "raju", "8555058332", "adoni", "andhra", "518301");
+        assertEquals(true,obj.personbyCity("adoni"));
+    }
+    }
